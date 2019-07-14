a46c00e1be3a01f870777a2c0b8e4b4919292c7d
Henadz Varantsou <henadz.varantsou@gmail.com>
2019-07-14 14:06:54 +0300

    Initial commit

---
+++ ctd-0160-rc3/src/main/scala/com/henry/ctd/Main.scala
@@ -0,0 +1,10 @@
+package com.henry.ctd
+
+import delegate com.henry.ctd.instance._
+import com.henry.ctd.common._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
